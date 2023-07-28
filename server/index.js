03a0f04cadc4a4b5854d0242006e9b2788cb0373
Tim Engle <timothy.englew@gmail.com>
2023-07-27 23:02:00 -0700

    🚀Init

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+// Packages
+const morgan = require('morgan');
+
+// Variables
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () => {
+  console.log(`Connected and listening on ${PORT}`);
+});
