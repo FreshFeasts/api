6aef5fea5e790975851bff307efa352a0669e234
Tim Engle <timothy.englew@gmail.com>
2023-07-30 00:12:42 -0700

    🍱 Add GET request for Meals

--- server/router.js
+++ server/router.js
@@ -16,4 +16,7 @@ router.get('/orders/user/:userId', controllers.Orders.getOrdersByUserId);
 //CC
 router.get('/cc/user/:userId', controllers.CreditCards.getCardsByUserId);
 
+// Meals
+router.get('/meals', controllers.Meals.getMeals);
+
 module.exports = router;
