e0442da21059fb77fa065e125ed553df8c4fb6d9
Tim Engle <timothy.englew@gmail.com>
2023-07-31 11:02:38 -0700

    🔌 Connect to remote DB

--- server/db/index.js
+++ server/db/index.js
@@ -3,10 +3,12 @@ const { MongoClient } = require('mongodb');
 
 const USERNAME = encodeURIComponent(process.env.DB_USER);
 const PASSWORD = encodeURIComponent(process.env.DB_PASS);
-const HOST = process.env.DB_HOST || 'localhost:27017';
+const HOST = process.env.DB_HOST || 'localhost';
+const PORT = process.env.DB_PORT || '27017';
 const DB_NAME = process.env.DB_NAME || 'freshfeast';
-// const URI = `mongodb://${USERNAME}:${PASSWORD}@${HOST}/?authMechanism=DEFAULT&authSource=${DB_NAME}`;
-const URI = 'mongodb://127.0.0.1:27017';
+const DB_AUTHCOLL = process.env.DB_AUTHCOLL || 'admin';
+const URI = `mongodb://${USERNAME}:${PASSWORD}@${HOST}:${PORT}/?authMechanism=DEFAULT&authSource=${DB_AUTHCOLL}`;
+// const URI = 'mongodb://127.0.0.1:27017';
 let db;
 
 const connectDb = async () => {
