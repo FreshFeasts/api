655a654e4fe7240f331dac289568e56aadd4078f
Tim Engle <timothy.englew@gmail.com>
2023-08-02 20:58:52 -0700

    💬 Update chate to use "chat message"

--- chat/server.js
+++ chat/server.js
@@ -1,42 +1,20 @@
-require('dotenv').config();
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-const port = process.env.CHAT_PORT || 3005;
-const io = require('socket.io')(port);
-console.log('Server is listening on port: %d', port);
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-io.of('/').on('connect', (socket) => {
-  console.log('\nA client connected');
-
-  socket.on('join', (data) => {
-    console.log('\n%s', data);
-    console.log('Nickname: ', data.sender, ', ID: ', socket.id);
-    console.log('Number of clients: %d', io.of('/').server.engine.clientsCount);
-    socket.nickname = data.sender;
-    socket.broadcast.emit('join', data);
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
+});
 
-  socket.on('list', (data) => {
-    console.log('\n%s', data);
-    var users = [];
-    for (const [key, value] of io.of('/').sockets) {
-      users.push(value.nickname);
-    }
-    socket.emit('list', { sender: data.sender, action: 'list', users: users });
-  });
-
-  socket.on('broadcast', (data) => {
-    console.log('\n%s', data);
-    socket.broadcast.emit('broadcast', data);
-  });
-
-  socket.on('quit', (data) => {
-    console.log('\n%s', data);
-    socket.broadcast.emit('quit', data);
-    socket.disconnect(true);
-  });
-
-  socket.on('disconnect', (reason) => {
-    console.log('\nA client disconnected, reason: %s', reason);
-    console.log('Number of clients: %d', io.of('/').server.engine.clientsCount);
-  });
+server.listen(3005, function () {
+  console.log('listening on *:3005');
 });
