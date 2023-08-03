e43b86498de24dea81f6897359a53a3e1d675afe
Tim Engle <timothy.englew@gmail.com>
2023-08-03 14:04:25 -0700

    💬 Update Chat Server

--- chat/server.js
+++ chat/server.js
@@ -8,10 +8,31 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-  console.log('a user connected');
+  console.log('Socket: ', socket);
+  console.log(`A user connected to server, with socketId of ${socket.id}`);
+
+  // Register
+  socket.on('register', (userId) => {
+    // Associate the user ID with the socket
+    socket.userId = userId;
+
+    // You can now use socket.userId to recognize this user
+    // across different events during this connection
+  });
+
+  // Chat Message
   socket.on('chat message', function (msg) {
-    console.log('message: ' + msg);
+    console.log('Sender', msg.sender);
+    console.log('Recepient', msg.recipient);
+    console.log('message: ' + msg.msg);
     io.emit('chat message', msg);
+  });
+
+  // Quit
+  socket.on('quit', (data) => {
+    console.log('\n%s', data);
+    socket.broadcast.emit('quit', data);
+    socket.disconnect(true);
   });
 });
 
